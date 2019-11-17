ca20dc54c40c7e9fed3931e9e3f468935e9ebbd6
Tracy Wong <>
2019-11-17 14:57:25 +0800

    Project Cell now accept props

--- client/src/components/grid/grid.component.jsx
+++ client/src/components/grid/grid.component.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { GridContainer } from './grid.styles';
 import ProjectCell from '../project-cell/project-cell.component';
 
-const Grid = () => (
-  <GridContainer>
-    <ProjectCell />
-    <ProjectCell />
-    <ProjectCell />
-    <ProjectCell />
-  </GridContainer>
-);
+const Grid = () => {
+  const item = {
+    imageUrl: '/images/dummy/project_thumb.jpg',
+    title: '企劃名稱',
+    date: '01/01/2020',
+    cmt: 99,
+    fav: 99
+  };
+  return (
+    <GridContainer>
+      <ProjectCell key={1} item={item} />
+      <ProjectCell key={2} item={item} />
+      <ProjectCell key={3} item={item} />
+      <ProjectCell key={4} item={item} />
+    </GridContainer>
+  );
+};
 
 export default Grid;
--- client/src/components/project-cell/project-cell.component.jsx
+++ client/src/components/project-cell/project-cell.component.jsx
@@ -10,23 +10,26 @@ import {
   ProjectCellFooterCnt
 } from './project-cell.styles';
 
-const ProjectCell = () => (
-  <ProjectCellContainer imageUrl="'/images/dummy/project_thumb.jpg'">
-    <ProjectCellHeader className="header">
-      <ProjectCellHeaderTitle>企劃名稱</ProjectCellHeaderTitle>
-      <ProjectCellHeaderDate>01/01/2020</ProjectCellHeaderDate>
-    </ProjectCellHeader>
-    <ProjectCellFooter className="footer">
-      <ProjectCellFooterCell>
-        <ProjectCellFooterImage src="/images/ico_comment_h13.png" />
-        <ProjectCellFooterCnt>99</ProjectCellFooterCnt>
-      </ProjectCellFooterCell>
-      <ProjectCellFooterCell>
-        <ProjectCellFooterImage src="/images/ico_favourite_h13.png" />
-        <ProjectCellFooterCnt>99</ProjectCellFooterCnt>
-      </ProjectCellFooterCell>
-    </ProjectCellFooter>
-  </ProjectCellContainer>
-);
+const ProjectCell = ({ item }) => {
+  const { imageUrl, title, date, cmt, fav } = item;
+  return (
+    <ProjectCellContainer imageUrl={imageUrl}>
+      <ProjectCellHeader className="header">
+        <ProjectCellHeaderTitle>{title}</ProjectCellHeaderTitle>
+        <ProjectCellHeaderDate>{date}</ProjectCellHeaderDate>
+      </ProjectCellHeader>
+      <ProjectCellFooter className="footer">
+        <ProjectCellFooterCell>
+          <ProjectCellFooterImage src="/images/ico_comment_h13.png" />
+          <ProjectCellFooterCnt>{cmt}</ProjectCellFooterCnt>
+        </ProjectCellFooterCell>
+        <ProjectCellFooterCell>
+          <ProjectCellFooterImage src="/images/ico_favourite_h13.png" />
+          <ProjectCellFooterCnt>{fav}</ProjectCellFooterCnt>
+        </ProjectCellFooterCell>
+      </ProjectCellFooter>
+    </ProjectCellContainer>
+  );
+};
 
 export default ProjectCell;
